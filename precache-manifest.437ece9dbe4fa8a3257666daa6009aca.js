d942d33d0370727ccb18f6d0e5cabcb7318d8f75
liming <759297569@qq.com>
2021-10-03 18:58:33 +0800

    deploy

---
+++ precache-manifest.437ece9dbe4fa8a3257666daa6009aca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8523364ea4cc88546e",
+    "url": "/jizhang-1-website/css/app.f5ac94bf.css"
+  },
+  {
+    "revision": "4a81033ed9c6a65dd360e63adacee0e6",
+    "url": "/jizhang-1-website/index.html"
+  },
+  {
+    "revision": "1c8523364ea4cc88546e",
+    "url": "/jizhang-1-website/js/app.69cc2ea7.js"
+  },
+  {
+    "revision": "d25a6fd7e2f0cc870907",
+    "url": "/jizhang-1-website/js/chunk-vendors.fb1b94a1.js"
+  },
+  {
+    "revision": "c85dfb4aeda956607145765b7ef84a12",
+    "url": "/jizhang-1-website/manifest.json"
+  },
+  {
+    "revision": "7dada8dfd9934ede839850adbbdadb1f",
+    "url": "/jizhang-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jizhang-1-website/robots.txt"
+  }
+]);
